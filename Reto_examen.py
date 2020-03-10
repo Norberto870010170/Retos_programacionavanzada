d2aba691b410471d0a7b4a8f0c376bc7d208d38f
Norberto870010170 <60890865+Norberto870010170@users.noreply.github.com>
2020-03-10 13:29:31 -0600

    Create Reto_examen.py
    Equipo 4
    Ada Barrera
    Norberto Rodriguez
    Abiel Villareal

---
+++ Reto_examen.py
@@ -0,0 +1,18 @@
+def solucion(A):
+    n=len(A)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if A[j] > A[j+1]:
+                A[j], A[j+1] = A[j+1], A[j]
+    count=0
+    for p in range(len(A)):
+        if A[p] == p+1:
+            count=count+1
+        if A[p] != p+1:
+            a=0
+    if count==len(A):
+        print(1)
+    else:
+        print(0)
+
+solucion([2,3,1,4,5,7,6,8,9])
